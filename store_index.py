edb42408e248b8ab84b19d64a8d59bc788c1fcc3
SudhaDevarakonda <153428418+SudhaDevarakonda@users.noreply.github.com>
2025-10-20 13:29:33 +0530

    Update store_index.py

--- store_index.py
+++ store_index.py
@@ -9,8 +9,6 @@ load_dotenv()
 
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-GEMINI_API_KEY=os.environ.get('GEMINI_API_KEY')
-os.environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
 
 extracted_data=load_pdf_file("C:/Users/chakr/OneDrive/Desktop/backend/sudha/MedicalChatbot/Data")
 text_chunks=text_split(extracted_data)
